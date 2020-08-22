23a82364a794157a98f6e5f3ebbd4abd98e3c612
Sam Verschueren <sam.verschueren@gmail.com>
2020-08-22 20:03:03 +0200

    Add support for Alfred 4 and fix opening second instance - fixes #4 #5

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const path = require('path');
 const fs = require('fs');
+const {spawn} = require('child_process');
 const resolveAlfredPrefs = require('resolve-alfred-prefs');
 const readPkg = require('read-pkg');
 const latestVersion = require('latest-version');
@@ -10,47 +11,66 @@ const utils = require('./lib/utils');
 
 const output = [];
 
-const update = pkg => execa('npm', ['install', '-g', pkg.name]).catch(error => {
-	output.push(error.message);
-});
+const update = async pkg => {
+	try {
+		return await execa('npm', ['install', '-g', pkg.name]);
+	} catch (error) {
+		output.push(error.message);
+	}
+};
 
-const checkAndUpdate = filePath => readPkg(filePath)
-	.then(pkg => {
-		if (!pkg.name || !pkg.version) {
-			return;
+const checkAndUpdate = async filePath => {
+	const pkg = await readPkg(filePath);
+
+	if (!pkg.name || !pkg.version) {
+		return;
+	}
+
+	try {
+		const version = await latestVersion(pkg.name);
+
+		if (semver.gt(version, pkg.version)) {
+			return update(pkg);
 		}
+	} catch (_) {
+		// Do nothing
+	}
+};
 
-		return latestVersion(pkg.name)
-			.then(version => {
-				if (semver.gt(version, pkg.version)) {
-					return update(pkg);
-				}
-			})
-			.catch(() => { });
-	});
+(async () => {
+	let alfredVersion;
 
-resolveAlfredPrefs()
-	.then(prefs => {
-		const workflowDir = path.join(prefs, 'workflows');
+	try {
+		const alfredPreferences = await resolveAlfredPrefs();
+
+		alfredVersion = alfredPreferences.version || 4;
+
+		const workflowDir = path.join(alfredPreferences.path, 'workflows');
 
 		// Retrieve all the symlinks from the workflows directory
-		return utils.findSymlinks(workflowDir);
-	})
-	.then(filePaths => {
+		const filePaths = await utils.findSymlinks(workflowDir);
+
 		// Iterate over all the workflows, check if they are outdated and update them
 		const promises = filePaths.map(filePath => checkAndUpdate(filePath));
 
-		return Promise.all(promises);
-	})
-	.then(result => {
+		const result = await Promise.all(promises);
+
 		if (output.length > 0) {
 			throw new Error(output.join('\n'));
 		}
 
 		console.log(utils.toMessage(result.filter(Boolean).length));
-	})
-	.catch(error => {
+	} catch (error) {
 		fs.writeFileSync('output', error.message);
 		console.log('Something went wrong');
-	})
-	.then(() => execa('open', ['-n', '-a', 'Alfred 3']));
+	} finally {
+		// Kill the Alfred application and restart it
+		const process = spawn(`pkill Alfred && open -n -a 'Alfred ${alfredVersion}'`, {
+			detached: true,
+			stdio: 'ignore',
+			shell: true
+		});
+
+		process.unref();
+	}
+})();
